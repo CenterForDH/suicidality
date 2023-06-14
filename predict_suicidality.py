348eebf0ac7a2d3433cd2b254c591ab0786c4155
CenterForDH <136561789+CenterForDH@users.noreply.github.com>
2023-06-14 17:51:34 +0900

    Update predict_suicidality.py

--- predict_suicidality.py
+++ predict_suicidality.py
@@ -4,53 +4,94 @@ import time
 
 st.set_page_config(layout="wide")
 
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
-
 @st.cache_data
 def model_file():
     mfile = 'finalized_model_adb.pkl'
     model = pickle.load(open(mfile, 'rb'))
-    
+
     return model
+
 
 def prediction(X_test):
     model = model_file()
     result = model.predict_proba([X_test])
-    
+
     return result[0][1]
 
 
-def input_values():
-    sex     = st.slider('SEX',                  1,   2,   1)
-    age     = st.slider('AGE',                 13,  18,  15)
-    bmi     = st.slider('BMI',                  1,   4,   1)
-    region  = st.slider('REGION',               1,   2,   1)
-    educa   = st.slider('EDUCATION',            1,   2,   1)
-    acad    = st.slider('ACADEMIC ACHIEVEMENT', 5,  10,   6)
-    econo   = st.slider('ECONOMIC STATUS',      1,   4,   1)
-    parent  = st.slider('PARENT EDUCATION',     1,   4,   1)
-    smoke   = st.slider('SMOKING STATUS',       0,   1,   1)
-    alcohol = st.slider('ACOHOL CONSUMPTION',    0,   4,   0)
-    stress  = st.slider('STRESS',               6,   9,   6)
-    depress = st.slider('DEPRESSION',           0,   1,   0)
-    derma   = st.slider('DERMATIS',             0,   1,   0)
-    asthma  = st.slider('ASTHMA',               0,   1,   0)
-    aad     = derma | asthma
+def set_bmi(bmi):
+    x = 4
+    if   bmi <  18.5           : x = 1
+    elif bmi >= 18.5 and x < 23: x = 2
+    elif bmi >= 23   and x < 25: x = 3
+    elif bmi >= 25             : x = 4
 
-    cols = ['SEX', 'age', 'bmi_2', 'region', 'education',
-            'academic achievement', 'economic', 'parental education',
-            'smoking', 'alcoholic consumption',
-            'stress', 'depression', 'derma', 'asthma', 'aad']
+    return x
+
+
+def input_values():
+    sex     = st.radio('SEX',('Male','Female'), horizontal=True)
+    sexDict = {'Male':1,'Female':2}
+    sex = sexDict[sex]
+
+    age     = st.radio('AGE (year)',(13,14,15,16,17,18), horizontal=True)
+
+    height  = st.number_input('Height (cm)',130)
+    weight  = st.number_input('Weight (kg)',50)
+    bmi = weight/((height/100)**2)
+    st.write('BMI ', round(bmi,2))
+    bmi = set_bmi(bmi)
     
-    X_test = [sex,age,bmi,region,educa,acad,econo,parent,
+    region  = st.radio('REGION', ('Urban','Rural'), horizontal=True)
+    regionDict = {'Urban':1,'Rural':2}
+    region  = regionDict[region]
+    
+    educa   = st.radio('EDUCATION', ('Middle schole','High school'), horizontal=True)
+    educaDict = {'Middle schole':1,'High school':2}
+    educa = educaDict[educa]
+    
+    acad    = st.radio('ACADEMIC ACHIEVEMENT', ('Low', 'Low-middle','Middle','Upper-middle','Upper'), horizontal=True)
+    acadDict = {'Low':10, 'Low-middle':9,'Middle':8,'Upper-middle':7,'Upper':6}
+    acad = acadDict[acad]
+    
+    income   = st.radio('HOUSEHOLD INCOME',('1Q (Lowest)','2Q','3Q','4Q (Highest)'), horizontal=True)
+    incomeDict = {'1Q (Lowest)':1,'2Q':2,'3Q':3,'4Q (Highest)':4}
+    income = incomeDict[income]
+    
+    parent  = st.radio('PARENT EDUCATION (GRADUATION)', ('Middle school or lower','High school','University or higher'), horizontal=True)
+    parentDict = {'Middle school or lower':1,'High school':2,'University or higher':3}
+    parent  = parentDict[parent]
+    
+    smoke   = st.radio('SMOKING STATUS', ('No','Yes'), horizontal=True)
+    smokeDict = {'No':0,'Yes':1}
+    smoke   = smokeDict[smoke]
+    
+    alcohol = st.radio('ACHOL CONSUMPTION PER MONTH', ('No','1-2','3-5','6-9','< 10'), horizontal=True)
+    alcoholDict = {'No':0,'1-2':1,'3-5':2,'6-9':4,'< 10':5}
+    alcohol = alcoholDict[alcohol]
+    
+    stress  = st.radio('STRESS', ('Mild','Moderate','High','Severe'), horizontal=True)
+    stressDict = {'Mild':6,'Moderate':7,'High':8,'Severe':9}
+    stress = stressDict[stress]
+    
+    depress = st.radio('DEPRESSION', ('No','Yes'), horizontal=True)
+    depressDict = {'No':0,'Yes':1}
+    depress = depressDict[depress]
+    
+    derma   = st.radio('DERMATIS', ('No','Yes'), horizontal=True)
+    dermaDict = {'No':0,'Yes':1}
+    derma   = dermaDict[derma]
+    
+    asthma  = st.radio('ASTHMA', ('No','Yes'), horizontal=True)
+    asthmaDict = {'No':0,'Yes':1}
+    asthma  = asthmaDict[asthma] 
+
+    aad     = derma | asthma
+    
+    X_test = [sex,age,bmi,region,educa,
+              acad,income,parent,
               smoke,alcohol,stress,depress,derma,asthma,aad]
-    
+
     result = prediction(X_test)
 
     return result
