bc33cf3e8085bfe6f239e70d6aef57098d359284
CenterForDH <136561789+CenterForDH@users.noreply.github.com>
2023-07-18 22:19:35 +0900

    Update predict_suicidality.py

--- predict_suicidality.py
+++ predict_suicidality.py
@@ -58,11 +58,11 @@ def set_bmi(bmi):
 
 
 def input_values():
-    sex     = st.radio('SEX',('Male','Female'), horizontal=True)
+    sex     = st.radio('Sex',('Male','Female'), horizontal=True)
     sexDict = {'Male':1,'Female':2}
     sex = sexDict[sex]
 
-    age     = st.radio('AGE (year)',(13,14,15,16,17,18), horizontal=True)
+    age     = st.radio('Age (year)',(13,14,15,16,17,18), horizontal=True)
 
     height  = st.number_input('Height (cm)', min_value=80, max_value=190, value=130)
     weight  = st.number_input('Weight (kg)', min_value=30, max_value=100, value=50)
@@ -71,47 +71,46 @@ def input_values():
     bmiDict = {1:'Underweight',2:'Normal',3:'Overweight',4:'Obesity'}
     st.write('BMI: ', bmiDict[bmi], round(bmiv,2))
     
-    region  = st.radio('REGION', ('Urban','Rural'), horizontal=True)
+    region  = st.radio('Region of regidence', ('Urban','Rural'), horizontal=True)
     regionDict = {'Urban':1,'Rural':2}
     region  = regionDict[region]
     
-    educa   = st.radio('EDUCATION', ('Middle school','High school'), horizontal=True)
+    educa   = st.radio('Grade', ('Middle school','High school'), horizontal=True)
     educaDict = {'Middle school':1,'High school':2}
     educa = educaDict[educa]
     
-    acad    = st.radio('ACADEMIC ACHIEVEMENT (Grade)', ('Low', 'Low-middle','Middle','Upper-middle','Upper'), horizontal=True)
+    acad    = st.radio('School performance', ('Low', 'Low-middle','Middle','Upper-middle','Upper'), horizontal=True)
     acadDict = {'Low':10, 'Low-middle':9,'Middle':8,'Upper-middle':7,'Upper':6}
     acad = acadDict[acad]
     
-    income   = st.radio('HOUSEHOLD INCOME',('1Q (Lowest)','2Q','3Q','4Q (Highest)'), horizontal=True)
+    income   = st.radio('Household income',('1Q (Lowest)','2Q','3Q','4Q (Highest)'), horizontal=True)
     incomeDict = {'1Q (Lowest)':1,'2Q':2,'3Q':3,'4Q (Highest)':4}
     income = incomeDict[income]
     
-    parent  = st.radio('PARENT EDUCATION (GRADUATION)', ('Middle school or lower','High school','University or higher','Unknown'), horizontal=True)
+    parent  = st.radio('Parents\' highest education level', ('Middle school or lower','High school','University or higher','Unknown'), horizontal=True)
     parentDict = {'Middle school or lower':4,'High school':3,'University or higher':2,'Unknown':1}
     parent  = parentDict[parent]
     
-    smoke   = st.radio('SMOKING STATUS', ('No','Yes'), horizontal=True)
+    smoke   = st.radio('Smoking status', ('No','Yes'), horizontal=True)
     smokeDict = {'No':0,'Yes':1}
     smoke   = smokeDict[smoke]
     
-    alcohol = st.radio('ACOHOL CONSUMPTION PER MONTH', ('No','1-2','3-5','6-9','< 10'), horizontal=True)
+    alcohol = st.radio('Acohol consumption per month', ('No','1-2','3-5','6-9','< 10'), horizontal=True)
     alcoholDict = {'No':0,'1-2':1,'3-5':2,'6-9':3,'< 10':4}
     alcohol = alcoholDict[alcohol]
     
-    stress  = st.radio('STRESS', ('Low','Moderate', 'High','Very much'), horizontal=True)
+    stress  = st.radio('Stress status', ('Low','Moderate', 'High','Very much'), horizontal=True)
     stressDict = {'Low':1,'Moderate':2,'High':3,'Very much':4}
     stress = stressDict[stress]
     
-    depress = st.radio('DEPRESSION', ('No','Yes'), horizontal=True)
+    depress = st.radio('Sadness and depair', ('No','Yes'), horizontal=True)
     depressDict = {'No':0,'Yes':1}
     depress = depressDict[depress]
     
-    derma   = st.radio('DERMATIS', ('No','Yes'), horizontal=True)
+    derma   = st.radio('Dermatis', ('No','Yes'), horizontal=True)
     dermaDict = {'No':0,'Yes':1}
     derma   = dermaDict[derma]
     
-    asthma  = st.radio('ASTHMA', ('No','Yes'), horizontal=True)
     asthmaDict = {'No':0,'Yes':1}
     asthma  = asthmaDict[asthma] 
 
