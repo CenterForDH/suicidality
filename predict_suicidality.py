70ebcd0a8f60aaf15cf060b91e8c3c795c5e44d2
CenterForDH <136561789+CenterForDH@users.noreply.github.com>
2023-07-27 00:54:23 +0900

    Update predict_suicidality.py

--- predict_suicidality.py
+++ predict_suicidality.py
@@ -4,6 +4,19 @@ import time
 
 st.set_page_config(layout="wide")
 
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 footerText = """
 <style>
 #MainMenu {
