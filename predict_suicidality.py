c5d018ea40ff50ff21f4a29ae7b0f61461cebd3b
CenterForDH <136561789+CenterForDH@users.noreply.github.com>
2023-12-20 09:02:20 +0900

    Update predict_suicidality.py

--- predict_suicidality.py
+++ predict_suicidality.py
@@ -47,7 +47,7 @@ st.markdown(str(footerText), unsafe_allow_html=True)
 
 @st.cache_data
 def model_file():
-    mfile = 'finalized_model_adb.pkl'
+    mfile = 'suicialthinking_final.pkl'
     model = pickle.load(open(mfile, 'rb'))
 
     return model
